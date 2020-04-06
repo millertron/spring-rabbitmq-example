4f108958edda66d1e4d16af6b9f1fd247f2d92ad
Koha Choji <koha.choji@vistair.com>
2020-04-06 07:48:01 +0100

    Initial commit

---
+++ src/test/java/com/millertronics/springrabbitmqexample/SpringRabbitmqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millertronics.springrabbitmqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
