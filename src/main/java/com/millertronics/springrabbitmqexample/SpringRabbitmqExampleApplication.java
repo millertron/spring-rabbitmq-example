cff0909d208173cc723d8b0e5b1529c7e964e093
Koha Choji <koha.choji@vistair.com>
2020-04-06 10:40:14 +0100

    Correct imported class

--- src/main/java/com/millertronics/springrabbitmqexample/SpringRabbitmqExampleApplication.java
+++ src/main/java/com/millertronics/springrabbitmqexample/SpringRabbitmqExampleApplication.java
@@ -1,10 +1,10 @@
 package com.millertronics.springrabbitmqexample;
 
-import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
